2375d2d5c96fcc277cc8decffe97d4b1b3dbdb81
aloogladys <aloogladys@gmail.com>
2023-12-13 13:29:52 +0300

    login_page

---
+++ echo_note/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from echo_app.views import sign_in
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', sign_in),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
